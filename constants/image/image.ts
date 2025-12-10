134e7a6f86e38668d8645cd7ffbb80b050b007a0
nguyenkienminh <kienminh04122003@gmail.com>
2025-12-10 15:58:10 +0700

    #23 fix

--- constants/image/image.ts
+++ constants/image/image.ts
@@ -5,7 +5,7 @@ import register from '../../assets/images/auth/register.png';
 import emptyCompany from '../../assets/images/company/empty.png';
 import logoFusion from '../../assets/images/logoFusion/Logo and company.png';
 import google from '../../assets/images/logoFusion/icons8-google-96.png';
-import emptyMember from '../../assets/images/member/empty-member.png';
+import emptyMember from '../../assets/images/member/empty_member.png';
 import emptyPartner from '../../assets/images/partner/partner.png';
 import changePassword from '../../assets/images/password/change_password.png';
 import requireEmail from '../../assets/images/password/require_mail.png';
